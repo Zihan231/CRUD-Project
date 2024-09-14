4e7b247ab88bdda9c6a6471991d14b3ca21cb7b0
Zihan231 <114851871+Zihan231@users.noreply.github.com>
2024-09-14 20:07:33 +0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace CRUDProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxID = new System.Windows.Forms.TextBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxSalary = new System.Windows.Forms.TextBox();
+            this.textBoxCity = new System.Windows.Forms.TextBox();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Historic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(180, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(356, 54);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CRUD Application";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Historic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(167, 35);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Employee ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Historic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 198);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(211, 35);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Employee Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Historic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 248);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(212, 35);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Employee Salary";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Historic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 303);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(185, 35);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Employee City";
+            // 
+            // textBoxID
+            // 
+            this.textBoxID.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.textBoxID.Location = new System.Drawing.Point(254, 148);
+            this.textBoxID.Name = "textBoxID";
+            this.textBoxID.Size = new System.Drawing.Size(282, 32);
+            this.textBoxID.TabIndex = 5;
+            this.textBoxID.TextChanged += new System.EventHandler(this.textBoxID_TextChanged);
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.textBoxName.Location = new System.Drawing.Point(254, 201);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(342, 32);
+            this.textBoxName.TabIndex = 6;
+            this.textBoxName.TextChanged += new System.EventHandler(this.textBoxName_TextChanged);
+            // 
+            // textBoxSalary
+            // 
+            this.textBoxSalary.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.textBoxSalary.Location = new System.Drawing.Point(254, 251);
+            this.textBoxSalary.Name = "textBoxSalary";
+            this.textBoxSalary.Size = new System.Drawing.Size(214, 32);
+            this.textBoxSalary.TabIndex = 7;
+            // 
+            // textBoxCity
+            // 
+            this.textBoxCity.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.textBoxCity.Location = new System.Drawing.Point(254, 307);
+            this.textBoxCity.Name = "textBoxCity";
+            this.textBoxCity.Size = new System.Drawing.Size(342, 32);
+            this.textBoxCity.TabIndex = 8;
+            this.textBoxCity.TextChanged += new System.EventHandler(this.textBoxCity_TextChanged);
+            // 
+            // btnFind
+            // 
+            this.btnFind.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnFind.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFind.Location = new System.Drawing.Point(573, 145);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(100, 35);
+            this.btnFind.TabIndex = 9;
+            this.btnFind.Text = "Find";
+            this.btnFind.UseVisualStyleBackColor = false;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnSave.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(18, 368);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(100, 35);
+            this.btnSave.TabIndex = 10;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnUpdate.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(263, 368);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(100, 35);
+            this.btnUpdate.TabIndex = 11;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnDelete.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(496, 368);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(100, 35);
+            this.btnDelete.TabIndex = 12;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(37, 433);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersWidth = 51;
+            this.dataGridView.RowTemplate.Height = 24;
+            this.dataGridView.Size = new System.Drawing.Size(605, 198);
+            this.dataGridView.TabIndex = 13;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(279, 645);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 35);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(680, 686);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnFind);
+            this.Controls.Add(this.textBoxCity);
+            this.Controls.Add(this.textBoxSalary);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.textBoxID);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxID;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxSalary;
+        private System.Windows.Forms.TextBox textBoxCity;
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUDProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoadGrid();
+        }
+        public void LoadGrid (){
+            SqlConnection con = new SqlConnection("Data Source=LAPTOP-J7JBFG4A\\SQLEXPRESS;Initial Catalog=EmpCRUD;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Select * from info",con);
+            DataTable dt = new DataTable();
+            SqlDataReader sdr = cmd.ExecuteReader();
+            dt.Load(sdr);
+            con.Close();
+            dataGridView.DataSource = dt.DefaultView;
+        }
+        public void ClearData()
+        {
+            textBoxID.Clear();
+            textBoxName.Clear();
+            textBoxSalary.Clear();
+            textBoxCity.Clear();
+
+        }
+        public bool IsValid()
+        {
+            if (textBoxID.Text == string.Empty)
+            {
+                MessageBox.Show("ID Is Required !!!");
+                return false;
+            }
+            if (textBoxName.Text == string.Empty)
+            {
+                MessageBox.Show("Name Is Required !!!");
+                return false;
+            }
+            if (textBoxSalary.Text == string.Empty)
+            {
+                MessageBox.Show("Salary Is Required !!!");
+                return false;
+            }
+            if (textBoxCity.Text == string.Empty)
+            {
+                MessageBox.Show("City Is Required !!!");
+                return false;
+            }
+
+            return true;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try{
+                if (IsValid())
+                {
+                    SqlConnection con = new SqlConnection("Data Source=LAPTOP-J7JBFG4A\\SQLEXPRESS;Initial Catalog=EmpCRUD;Integrated Security=True");
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand("insert into info values(@id,@name,@salary,@city)", con);
+                    cmd.Parameters.AddWithValue("@id", int.Parse(textBoxID.Text));
+                    cmd.Parameters.AddWithValue("@name", textBoxName.Text);
+                    cmd.Parameters.AddWithValue("@salary", double.Parse(textBoxSalary.Text));
+                    cmd.Parameters.AddWithValue("@city", textBoxCity.Text);
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+
+                    MessageBox.Show("Successfully Saved");
+                    ClearData();
+                    LoadGrid(); 
+
+                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        
+        }
+        private void textBoxName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (IsValid())
+                {
+
+                    SqlConnection con = new SqlConnection("Data Source=LAPTOP-J7JBFG4A\\SQLEXPRESS;Initial Catalog=EmpCRUD;Integrated Security=True");
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand("update info set name=@name,salary=@salary,city=@city where id=@id", con);
+                    cmd.Parameters.AddWithValue("@id",int.Parse(textBoxID.Text));
+                    cmd.Parameters.AddWithValue("@name", textBoxName.Text);
+                    cmd.Parameters.AddWithValue("@salary", double.Parse(textBoxSalary.Text));
+                    cmd.Parameters.AddWithValue("@city", textBoxCity.Text);
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    MessageBox.Show("Successfully Updated");
+                    LoadGrid();
+                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBoxID.Text != string.Empty)
+                {
+                    SqlConnection con = new SqlConnection("Data Source=LAPTOP-J7JBFG4A\\SQLEXPRESS;Initial Catalog=EmpCRUD;Integrated Security=True");
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand("Delete from info where id = @id",con);
+                    cmd.Parameters.AddWithValue("@id", textBoxID .Text);
+                    cmd.ExecuteNonQuery(); 
+                    MessageBox.Show("ID "+ textBoxID.Text+" is Deleted Successfully !!!");
+                    con.Close();
+                    LoadGrid();
+
+                }
+                else
+                {
+                    MessageBox.Show("ID is Required !!!");
+                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            try {
+                if (textBoxID.Text != string.Empty) {
+                    SqlConnection con = new SqlConnection("Data Source=LAPTOP-J7JBFG4A\\SQLEXPRESS;Initial Catalog=EmpCRUD;Integrated Security=True");
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand("Select * from info where id=@id", con);
+                    cmd.Parameters.AddWithValue("@id",textBoxID.Text);
+                    DataTable dt = new DataTable();
+                    SqlDataReader sdr = cmd.ExecuteReader();
+                    dt.Load(sdr);
+                    con.Close();
+                    dataGridView.DataSource = dt.DefaultView;
+                }
+                else
+                {
+                    MessageBox.Show("Id required !!!");
+                }               
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Display a confirmation message box
+            DialogResult result = MessageBox.Show(
+                "Are you sure you want to exit?",
+                "Exit Confirmation",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+
+            // Check if the user clicked 'Yes'
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit(); // Exit the application
+            }
+        }
+
+
+        private void textBoxCity_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBoxID_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
